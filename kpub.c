b9c60b61b60aa745525fe57c534293e62d15de3b
Andrew Bond <olishmollie@gmail.com>
2024-12-31 19:04:33 +0000

    MVP: does not support EOF condition

--- kpub.c
+++ kpub.c
@@ -5,26 +5,32 @@
 #include <linux/kernel.h>
 #include <linux/list.h>
 #include <linux/module.h>
+#include <linux/poll.h>
 #include <linux/printk.h>
 #include <linux/sysfs.h>
 #include <linux/uaccess.h>
+#include <linux/wait.h>
 
 #define NUM_TOPICS 256
 #define MAX_STR_LEN 256
 #define MAX_BUF_SIZE PAGE_SIZE
 
-struct kpub_topic {
+struct topic {
+	wait_queue_head_t inq, outq;
+	size_t size;
+	size_t nreaders, nwriters;
+	size_t wp;
+	char *buf;
+	char name[MAX_STR_LEN];
 	struct device dev;
 	struct cdev cdev;
 	struct list_head entry;
-	char name[MAX_STR_LEN];
-	uint16_t len;
-	uint16_t buf_size;
+	struct semaphore sem;
 };
 
-#define cdev_to_topic(ptr) container_of(ptr, struct kpub_topic, cdev);
-#define dev_to_topic(ptr) container_of(ptr, struct kpub_topic, dev);
-#define node_to_topic(ptr) list_entry(ptr, struct kpub_topic, entry);
+#define cdev_to_topic(ptr) container_of(ptr, struct topic, cdev);
+#define dev_to_topic(ptr) container_of(ptr, struct topic, dev);
+#define node_to_topic(ptr) list_entry(ptr, struct topic, entry);
 
 /* Stores all topics. */
 LIST_HEAD(topics);
@@ -44,31 +50,34 @@ static int major_num;
 /* Tracks minor numbers in use. */
 static uint8_t minor_nums[NUM_TOPICS];
 
+/* Read the topic name. */
 static ssize_t name_show(struct device *dev, struct device_attribute *attr,
 			 char *buf)
 {
-	struct kpub_topic *topic = dev_to_topic(dev);
-	memcpy(buf, topic->name, topic->len);
-	return topic->len;
+	struct topic *topic = dev_to_topic(dev);
+	size_t len = strlen(topic->name);
+	memcpy(buf, topic->name, len);
+	return len;
 }
 
-static ssize_t buf_size_show(struct device *dev, struct device_attribute *attr,
-			     char *buf)
+/* Read the topic buffer size. */
+static ssize_t size_show(struct device *dev, struct device_attribute *attr,
+			 char *buf)
 {
-	struct kpub_topic *topic = dev_to_topic(dev);
-	return snprintf(buf, MAX_STR_LEN, "%d", topic->buf_size);
+	struct topic *topic = dev_to_topic(dev);
+	return snprintf(buf, MAX_STR_LEN, "%lu", topic->size);
 }
 
 DEVICE_ATTR_RO(name);
-DEVICE_ATTR_RO(buf_size);
+DEVICE_ATTR_RO(size);
 static struct attribute *topic_attrs[] = {
 	&dev_attr_name.attr,
-	&dev_attr_buf_size.attr,
+	&dev_attr_size.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(topic);
 
-/* Return the first available minor number. */
+/* Reserve and return the first available minor number. */
 static int reserve_minor_num(void)
 {
 	int i;
@@ -92,7 +101,7 @@ static void release_minor_num(int minor_num)
 /* Release the topic's embedded device. */
 static void device_release(struct device *dev)
 {
-	// No need for releasing stack bound devices.
+	// No need to release stack bound devices, but Linux expects one to be defined.
 }
 
 /* Create a new topic by writing its name to the class attribute. */
@@ -101,7 +110,7 @@ static ssize_t create_topic_store(const struct class *cp,
 				  const char *buf, size_t len)
 {
 	int devt, err, minor_num;
-	struct kpub_topic *topic = NULL;
+	struct topic *topic;
 
 	if (len == 0) {
 		pr_alert("%s: topic cannot have an empty name\n",
@@ -115,9 +124,22 @@ static ssize_t create_topic_store(const struct class *cp,
 		return -EINVAL;
 	}
 
-	topic = (struct kpub_topic *)kzalloc(sizeof(*topic), GFP_KERNEL);
+	topic = (struct topic *)kzalloc(sizeof(*topic), GFP_KERNEL);
 	if (!topic)
 		return -ENOMEM;
+
+	topic->size = 9;
+	memcpy(topic->name, buf, len);
+
+	sema_init(&topic->sem, 1);
+	init_waitqueue_head(&topic->inq);
+	init_waitqueue_head(&topic->outq);
+
+	topic->buf = (char *)kzalloc(topic->size, GFP_KERNEL);
+	if (!topic->buf) {
+		err = -ENOMEM;
+		goto cleanup_topic;
+	}
 
 	minor_num = reserve_minor_num();
 	if (minor_num < 0) {
@@ -138,10 +160,6 @@ static ssize_t create_topic_store(const struct class *cp,
 			 THIS_MODULE->name);
 		goto cleanup_cdev;
 	}
-
-	topic->len = len;
-	topic->buf_size = PAGE_SIZE;
-	memcpy(topic->name, buf, len);
 
 	err = kobject_set_name(&topic->dev.kobj, "kpub!%s", topic->name);
 	if (err < 0) {
@@ -171,9 +189,21 @@ static ssize_t create_topic_store(const struct class *cp,
 cleanup_cdev:
 	cdev_del(&topic->cdev);
 cleanup_topic:
+	kfree(topic->buf);
 	kfree(topic);
 
 	return err;
+}
+
+/* Delete a topic and release its resources. */
+static void delete_topic(struct topic *topic)
+{
+	release_minor_num(topic->dev.id);
+	list_del(&topic->entry);
+	device_unregister(&topic->dev);
+	cdev_del(&topic->cdev);
+	kfree(topic->buf);
+	kfree(topic);
 }
 
 /* Remove a topic by writing its name to the class attribute. */
@@ -181,7 +211,7 @@ static ssize_t remove_topic_store(const struct class *cls,
 				  const struct class_attribute *attr,
 				  const char *buf, size_t len)
 {
-	struct kpub_topic *topic = NULL, *tp;
+	struct topic *topic = NULL, *tp;
 	struct list_head *node;
 
 	if (len >= MAX_STR_LEN)
@@ -198,11 +228,7 @@ static ssize_t remove_topic_store(const struct class *cls,
 	if (!topic)
 		return -ENODEV;
 
-	release_minor_num(topic->dev.id);
-	list_del(&topic->entry);
-	device_unregister(&topic->dev);
-	cdev_del(&topic->cdev);
-	kfree(topic);
+	delete_topic(topic);
 
 	pr_info("%s: removed topic '%s'\n", THIS_MODULE->name, buf);
 
@@ -214,10 +240,6 @@ static ssize_t remove_topic_store(const struct class *cls,
  *  - create_topic
  * 	- remove_topic
  */
-/*static struct class_attribute class_attr_create_topic =*/
-/*	__ATTR(create_topic, 0220, NULL, create_topic_store);*/
-/*static struct class_attribute class_attr_remove_topic =*/
-/*	__ATTR(remove_topic, 0220, NULL, remove_topic_store);*/
 CLASS_ATTR_WO(create_topic);
 CLASS_ATTR_WO(remove_topic);
 static struct attribute *class_attrs[] = {
@@ -233,56 +255,182 @@ static struct class kpub_class = {
 	.class_groups = class_groups,
 };
 
+/* Open the device for reading xor writing. */
 static int kpub_open(struct inode *inode, struct file *file)
 {
-	struct kpub_topic *topic;
-	topic = cdev_to_topic(inode->i_cdev);
-	pr_info("%s: opening topic '%s'\n", THIS_MODULE->name, topic->name);
-	return 0;
+	struct topic *topic = cdev_to_topic(inode->i_cdev);
+	int err = 0;
+
+	if (down_interruptible(&topic->sem))
+		return -ERESTARTSYS;
+
+	file->private_data = topic;
+
+	if (file->f_mode & FMODE_READ && !(file->f_mode & FMODE_WRITE)) {
+		++topic->nreaders;
+	} else if (file->f_mode & FMODE_WRITE && !(file->f_mode & FMODE_READ)) {
+		++topic->nwriters;
+		wake_up_interruptible(&topic->inq);
+	} else {
+		dev_err(&topic->dev,
+			"topic must be opened as reader xor writer");
+		err = -EACCES;
+	}
+
+	dev_info(&topic->dev, "updating file pos...\n");
+	file->f_pos = topic->wp;
+
+	up(&topic->sem);
+
+	return err;
 }
 
 static int kpub_release(struct inode *inode, struct file *file)
 {
-	struct kpub_topic *topic;
-	topic = cdev_to_topic(inode->i_cdev);
-	pr_info("%s: closing topic '%s'\n", THIS_MODULE->name, topic->name);
-	return 0;
+	struct topic *topic = cdev_to_topic(inode->i_cdev);
+	int err = 0;
+
+	if (down_interruptible(&topic->sem))
+		return -ERESTARTSYS;
+
+	file->private_data = topic;
+	dev_info(&topic->dev, "closing topic '%s'\n", topic->name);
+
+	if (file->f_mode & FMODE_READ && !(file->f_mode & FMODE_WRITE)) {
+		--topic->nreaders;
+	} else if (file->f_mode & FMODE_WRITE && !(file->f_mode & FMODE_READ)) {
+		--topic->nwriters;
+	} else {
+		dev_err(&topic->dev,
+			"topic must be opened as reader xor writer");
+		err = -EACCES;
+	}
+
+	up(&topic->sem);
+
+	return err;
 }
 
 static ssize_t kpub_read(struct file *file, char __user *buf, size_t len,
 			 loff_t *off)
 {
-	int err;
-	const char *msg = "hello world!\n";
-	size_t msg_len = strlen(msg);
+	struct topic *topic = file->private_data;
 
-	if (*off >= msg_len) {
+	if (down_interruptible(&topic->sem))
+		return -ERESTARTSYS;
+
+	dev_info(&topic->dev, "%s sleeping...\n", topic->name);
+	while (topic->wp == *off) {
+		up(&topic->sem);
+		if (file->f_flags & O_NONBLOCK)
+			return -EAGAIN;
+		if (wait_event_interruptible(topic->inq, (topic->wp != *off)))
+			return -ERESTARTSYS;
+		if (down_interruptible(&topic->sem))
+			return -ERESTARTSYS;
+	}
+
+	dev_info(&topic->dev, "info to read!\n");
+
+	if (topic->wp > *off)
+		len = min(len, (size_t)(topic->wp - *off));
+	else
+		len = min(len, (size_t)(topic->size - *off));
+
+	dev_info(&topic->dev, "copying %lu bytes to buffer\n", len);
+	if (copy_to_user(buf, &topic->buf[*off], len))
+		return -EFAULT;
+
+	*off += len;
+	if (*off >= topic->size)
 		*off = 0;
-		return 0;
-	}
 
-	err = copy_to_user(buf, msg, msg_len);
-	if (err < 0) {
-		pr_alert("%s: could not copy buffer to user space\n",
-			 THIS_MODULE->name);
-		return err;
-	}
+	up(&topic->sem);
 
-	*off += msg_len;
+	wake_up_interruptible(&topic->outq);
 
-	return msg_len;
+	return len;
+}
+
+/* Return the number of bytes available to be written to. */
+static size_t bytes_to_write(struct topic *topic, loff_t *off)
+{
+	if (topic->wp == *off)
+		return topic->size - 1;
+	if (topic->wp > *off)
+		return topic->wp - *off;
+	return topic->size + topic->wp - *off;
 }
 
 static ssize_t kpub_write(struct file *file, const char __user *buf, size_t len,
 			  loff_t *off)
 {
+	struct topic *topic = file->private_data;
+
+	if (len > topic->size) {
+		dev_err(&topic->dev, "%lu byte message is too large\n", len);
+		return -EINVAL;
+	}
+
+	if (down_interruptible(&topic->sem))
+		return -ERESTARTSYS;
+
+	while (bytes_to_write(topic, off) == 0) {
+		up(&topic->sem);
+		if (file->f_flags & O_NONBLOCK)
+			return -EAGAIN;
+		if (wait_event_interruptible(topic->outq,
+					     (bytes_to_write(topic, off) > 0)))
+			return -ERESTARTSYS;
+		if (down_interruptible(&topic->sem))
+			return -ERESTARTSYS;
+	}
+
+	if (topic->wp >= *off)
+		len = min(len, (size_t)(topic->size - topic->wp));
+	else
+		len = min(len, (size_t)(*off - topic->wp));
+
+	dev_info(&topic->dev, "copying %lu bytes from buf\n", len);
+	if (copy_from_user(&topic->buf[topic->wp], buf, len)) {
+		up(&topic->sem);
+		return -EFAULT;
+	}
+
+	topic->wp += len;
+	if (topic->wp == topic->size)
+		topic->wp = 0;
+
+	up(&topic->sem);
+
+	wake_up_interruptible(&topic->inq);
+
 	return len;
 }
 
-static __poll_t kpub_poll(struct file *file,
-			  struct poll_table_struct *poll_table)
+static unsigned kpub_poll(struct file *file, poll_table *ppt)
 {
-	return 0;
+	struct topic *topic = file->private_data;
+	int ready_mask = 0;
+
+	down(&topic->sem);
+
+	if (topic->nwriters == 0) {
+		up(&topic->sem);
+		return POLLHUP;
+	}
+
+	poll_wait(file, &topic->inq, ppt);
+	poll_wait(file, &topic->outq, ppt);
+
+	if (file->f_pos != topic->wp)
+		ready_mask |= (POLLIN | POLLRDNORM);
+	if (bytes_to_write(topic, &file->f_pos) > 0)
+		ready_mask |= POLLOUT | POLLWRNORM;
+
+	up(&topic->sem);
+
+	return ready_mask;
 }
 
 static struct file_operations kpub_fops = {
@@ -326,17 +474,13 @@ cleanup:
 static void __exit kpub_exit(void)
 {
 	struct list_head *node, *tmp;
-	struct kpub_topic *topic;
+	struct topic *topic;
 
 	list_for_each_safe(node, tmp, &topics) {
 		topic = node_to_topic(node);
 		pr_info("%s: removing topic '%s'\n", THIS_MODULE->name,
 			topic->name);
-		release_minor_num(topic->dev.id);
-		device_unregister(&topic->dev);
-		cdev_del(&topic->cdev);
-		list_del(&topic->entry);
-		kfree(topic);
+		delete_topic(topic);
 	}
 
 	class_unregister(&kpub_class);
